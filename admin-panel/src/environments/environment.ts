9c93b6d4c9cc8244a294edb659fa720ad06b5bd5
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2025-01-31 13:14:52 +0600

    add environment file

---
+++ admin-panel/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  country: 'BD',
+  production: false,
+  baseUrl: 'https://localhost:5050',
+  registerUrl: 'https://localhost:5050/Authentication/register',
+  loginUrl: 'https://localhost:5050/Authentication/login',
+  educationPost: 'https://localhost:5050/education/users/1EC6473A-3C18-49A5-A460-986879BB9CBE/create',
+};
