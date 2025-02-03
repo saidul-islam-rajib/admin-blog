a648683bf8a27aac1d3e57f79dad7c64e8c10252
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2025-02-03 14:56:56 +0600

    add new component for project

---
+++ admin-panel/src/app/views/about/projects/add-project/add-project.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-project',
+  imports: [],
+  templateUrl: './add-project.component.html',
+  styleUrl: './add-project.component.scss'
+})
+export class AddProjectComponent {
+
+}
