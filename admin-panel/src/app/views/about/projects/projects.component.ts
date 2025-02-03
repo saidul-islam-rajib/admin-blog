e9b124e00c276145afdd8685355ca8c95811bad3
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2025-02-03 15:08:31 +0600

    add projects

--- admin-panel/src/app/views/about/projects/projects.component.ts
+++ admin-panel/src/app/views/about/projects/projects.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { AboutApiService } from '../../../core/services/about-api.service';
+import { Project, ProjectSection } from '../../../core/interfaces/project';
 
 @Component({
   selector: 'app-projects',
   imports: [],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  public projectList: Project[] = [];
+  constructor(private about: AboutApiService) {
+    this.getProjectList();
+  }
 
+  getProjectList() {
+    this.about.getProjectList().subscribe({
+      next: (response) => {
+        this.projectList = response;
+      },
+      error: (err) => {
+        console.log('Error while loading project list', err.error.message);
+      },
+    });
+  }
+
+  getProjectSection(section: any) {
+    if (!section || !Array.isArray(section)) {
+      return '';
+    }
+    return section.map((s: any, index: number) => `${index + 1}. ${s.topicName}`)
+  }
+
+  getProjectTagSection(tag: any) {
+    if (!tag || !Array.isArray(tag)) {
+      return '';
+    }
+    return tag.map((k: any) => `${k.projectTagName}`);
+  }
 }
