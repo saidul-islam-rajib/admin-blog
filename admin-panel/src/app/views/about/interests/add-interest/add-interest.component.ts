40064f65dc93a959d30d3e8a80f36fca05f69abe
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2025-02-03 12:19:02 +0600

    add publication and interest component

---
+++ admin-panel/src/app/views/about/interests/add-interest/add-interest.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-interest',
+  imports: [],
+  templateUrl: './add-interest.component.html',
+  styleUrl: './add-interest.component.scss'
+})
+export class AddInterestComponent {
+
+}
---
+++ admin-panel/src/app/views/about/publications/add-publication/add-publication.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-publication',
+  imports: [],
+  templateUrl: './add-publication.component.html',
+  styleUrl: './add-publication.component.scss'
+})
+export class AddPublicationComponent {
+
+}
