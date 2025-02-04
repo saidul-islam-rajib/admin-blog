99deeacd2bfc5b16f1fa08920c0675f3274efab7
Saidul Islam Rajib <saidulislamrajib1997@gmail.com>
2025-02-04 17:35:31 +0600

    add post

--- admin-panel/src/app/views/post/add-post/add-post.component.ts
+++ admin-panel/src/app/views/post/add-post/add-post.component.ts
@@ -1,11 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { PostApiService } from '../../../core/services/post-api.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-post',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-post.component.html',
-  styleUrl: './add-post.component.scss'
+  styleUrl: './add-post.component.scss',
 })
-export class AddPostComponent {
+export class AddPostComponent implements OnInit {
+  educationForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private apiService: PostApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.educationForm = this.fb.group({
+      instituteName: ['', Validators.required],
+      instituteLogo: ['', Validators.required],
+      department: ['', Validators.required],
+      isCurrentStudent: [false],
+      startDate: ['', Validators.required],
+      endDate: [''],
+      educationSections: this.fb.array([this.createEducationSection()]),
+    });
+  }
+
+  get educationSections(): FormArray {
+    return this.educationForm.get('educationSections') as FormArray;
+  }
+
+  createEducationSection(): FormGroup {
+    return this.fb.group({
+      sectionDescription: ['', Validators.required],
+    });
+  }
+
+  addEducationSection(): void {
+    this.educationSections.push(this.createEducationSection());
+  }
+
+  removeEducationSection(index: number): void {
+    this.educationSections.removeAt(index);
+  }
+
+  onSubmit(): void {
+    if (this.educationForm.valid) {
+      console.log("valid form: ", this.educationForm.value);
+
+      this.apiService.getPostList().subscribe({
+        next: (response) => {
+          console.log("Response data list : ", response)
+        }
+      })
+    } else {
+      this.educationForm.markAllAsTouched();
+    }
+  }
 }
--- admin-panel/src/app/views/post/post.component.ts
+++ admin-panel/src/app/views/post/post.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../../core/interfaces/post';
+import { PostApiService } from '../../core/services/post-api.service';
 
 @Component({
   selector: 'app-post',
   imports: [],
   templateUrl: './post.component.html',
-  styleUrl: './post.component.scss'
+  styleUrl: './post.component.scss',
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  public postList: Post[] = [];
 
+  constructor(
+    private postService: PostApiService
+  ){
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  getPostList(){
+    this.postService.getPostList().subscribe({
+      next: (response) => {
+        console.log("List of post : ", response)
+        this.postList = response;
+      },
+      error: (err) => {
+        console.log("Error while loading list of posts", err);
+      }
+    });
+  }
 }
