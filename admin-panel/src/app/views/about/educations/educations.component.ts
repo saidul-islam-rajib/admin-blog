eafa739bf7c085966583bed508d45b8d8c6f406b
Rajib <saidul.is.rajib@gmail.com>
2025-02-27 17:03:45 +0600

    add toaster while deleting new post from education list page

--- admin-panel/src/app/views/about/educations/educations.component.ts
+++ admin-panel/src/app/views/about/educations/educations.component.ts
@@ -3,6 +3,8 @@ import { AboutApiService } from '../../../core/services/about-api.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { Education } from '../../../core/interfaces/education';
+import { NgToastService } from 'ng-angular-popup';
+import { Colors } from '../../notifications/toasters/toasters.component';
 
 @Component({
   selector: 'app-educations',
@@ -14,7 +16,10 @@ import { Education } from '../../../core/interfaces/education';
 export class EducationsComponent implements OnInit {
   public educationList: Education[] = [];
 
-  constructor(private about: AboutApiService, private router: Router) {}
+  constructor(
+    private about: AboutApiService,
+    private toast: NgToastService
+  ) {}
   ngOnInit(): void {
     this.getEducationList();
   }
@@ -30,16 +35,21 @@ export class EducationsComponent implements OnInit {
 
   deleteEducation(educationId: string) {
     if (educationId != null) {
-      this.educationList = this.educationList.filter((edu) => edu.educationId !== educationId);
-  
-      this.about.deleteEducation(educationId, '2A52F7D6-7EB0-44D8-8513-3572375E3613').subscribe({
-        next: () => {
-        },
-        error: () => {
-          this.getEducationList();
-        },
-      });
+      this.educationList = this.educationList.filter(
+        (edu) => edu.educationId !== educationId
+      );
+
+      this.about
+        .deleteEducation(educationId, '2A52F7D6-7EB0-44D8-8513-3572375E3613')
+        .subscribe({
+          next: () => {
+            this.toast.success(Colors.success, 'Deleted', 2000);
+          },
+          error: () => {
+            this.getEducationList();
+            this.toast.danger(Colors.danger, 'Failed to delete', 2000);
+          },
+        });
     }
   }
-  
 }
