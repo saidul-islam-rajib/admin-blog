79498e2bc85598e570c10aaf20913bcfd6182373
Rajib <saidulislamrajib1997@gmail.com>
2025-03-11 12:18:43 +0600

    add image uploader component

---
+++ admin-panel/src/app/shared/image-uploader/image-uploader.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-uploader',
+  imports: [],
+  templateUrl: './image-uploader.component.html',
+  styleUrl: './image-uploader.component.scss'
+})
+export class ImageUploaderComponent {
+
+}
